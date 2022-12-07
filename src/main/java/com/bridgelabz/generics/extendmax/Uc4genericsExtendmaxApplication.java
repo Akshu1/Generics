e276a04859a272fd8230e28fb7561edad4432973
akshu1 <akshathanagaraj98@gmail.com>
2022-12-07 18:48:54 +0530

    Add: Program to extend the method and to take more parameters

---
+++ src/main/java/com/bridgelabz/generics/extendmax/Uc4genericsExtendmaxApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.extendmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc4genericsExtendmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc4genericsExtendmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/generics/extendmax/Uc4genericsExtendmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.extendmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc4genericsExtendmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
