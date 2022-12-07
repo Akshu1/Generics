09c948aa141ae4483fdb78f7cc47fa429b283a0b
akshu1 <akshathanagaraj98@gmail.com>
2022-12-07 18:39:08 +0530

    Add:program to find maximum float value

---
+++ src/main/java/com/bridgelabz/generics/uc2generics/maxfloat/Uc2genericsMaxfloatApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.uc2generics.maxfloat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc2genericsMaxfloatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc2genericsMaxfloatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/generics/uc2generics/maxfloat/Uc2genericsMaxfloatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.uc2generics.maxfloat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc2genericsMaxfloatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
