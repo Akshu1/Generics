60bfd79543171b8de0f2e3cf99e718ecd5a11757
akshu1 <akshathanagaraj98@gmail.com>
2022-12-07 18:44:02 +0530

    Add:Program to find Maximum String value

---
+++ src/main/java/com/bridgelabz/generics/maxstring/Uc3genericsMaxStringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.maxstring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc3genericsMaxStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc3genericsMaxStringApplication.class, args);
+	}
+
+}
