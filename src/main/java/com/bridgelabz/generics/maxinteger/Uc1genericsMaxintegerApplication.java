267b8f24e7acefd8af4a0b5d11b266bb09d75a3e
akshu1 <akshathanagaraj98@gmail.com>
2022-12-07 18:31:38 +0530

    Add:Program to check Max Integer

---
+++ src/main/java/com/bridgelabz/generics/maxinteger/Uc1genericsMaxintegerApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.maxinteger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1genericsMaxintegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1genericsMaxintegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/generics/maxinteger/Uc1genericsMaxintegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics.maxinteger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc1genericsMaxintegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
