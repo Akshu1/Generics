b278133ab9a3fc0e25661dd69b3013d38558335e
akshu1 <akshathanagaraj98@gmail.com>
2022-12-07 18:54:09 +0530

    Add:Pgm print max to Print StdOut

---
+++ src/main/java/com/bridgelabz/generics/Uc5genericsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc5genericsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc5genericsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/generics/Uc5genericsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc5genericsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
